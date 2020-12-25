176995878a897f24ae9f1482095a323af8292cb7
Huy Nguyen <hello@huynguyen.me>
2020-12-25 00:46:57 -0600

    Added etcd_k3s_endpoint_builder

--- filter_plugins/filter.py
+++ filter_plugins/filter.py
@@ -1,13 +1,16 @@
 class FilterModule(object):
     def filters(self):
         return {
-            'k3s_exec_arg_builder': self.k3s_exec_arg_builder
+            'k3s_exec_arg_builder': self.k3s_exec_arg_builder,
+            'etcd_bootstrap_builder': self.etcd_bootstrap_builder,
+            'etcd_k3s_endpoint_builder': self.etcd_k3s_endpoint_builder
         }
 
 # self can be passed in here to reference other modules?
-    def k3s_exec_arg_builder(self, k3s_exec_type, k3s_cri_arg, apiserver_address, apiserver_port, advertise_address, advertise_port, tls_san, cluster_cidr, service_cidr, cluster_core_dns, cluster_domain):
+    def k3s_exec_arg_builder(self, k3s_exec_type, k3s_cri_arg, apiserver_address, apiserver_port, advertise_address, advertise_port, tls_san, cluster_cidr, service_cidr, cluster_core_dns, cluster_domain, k3s_server_coredns_enabled, k3s_server_servicelb_enabled, k3s_server_traefik_enabled, k3s_server_local_storage_enabled, k3s_server_metrics_server_enabled):
         # initial empty list
         k3s_exec_arg = [ ]
+        k3s_components = [ ]
 
         k3s_exec_arg.append( str(k3s_exec_type) + str(k3s_cri_arg) )
 
@@ -38,5 +41,69 @@ class FilterModule(object):
         if cluster_domain:
             k3s_exec_arg.append( "--cluster-domain " + str(cluster_domain) )
 
+        # forming the disabled parameter
+        if not k3s_server_coredns_enabled:
+            k3s_components.append( "coredns" )
+        
+        if not k3s_server_servicelb_enabled:
+            k3s_components.append( "servicelb" )
+        
+        if not k3s_server_traefik_enabled:
+            k3s_components.append( "traefik" )
+        
+        if not k3s_server_local_storage_enabled:
+            k3s_components.append( "local-storage" )
+        
+        if not k3s_server_metrics_server_enabled:
+            k3s_components.append( "metrics-server" )
+
+        if len(k3s_components) > 0:
+            k3s_exec_arg.append( "--disable " + ','.join(k3s_components) )
+
         # return joined string
         return ' '.join(k3s_exec_arg)
+    
+# string builder for bootstrap etcd nodes
+    def etcd_bootstrap_builder(self, blankignore, controlplane_hostnames, controlplane_ips, worker_hostnames, worker_ips, peer_port, ssl_enabled):
+        
+        ch = controlplane_hostnames.split(',')
+        cpi = controlplane_ips.split(',')
+        wh = worker_hostnames.split(',')
+        whi = worker_ips.split(',')
+        etcd_bootstrap_list = [ ]
+
+        if ssl_enabled:
+            protocol = "https://"
+        else:
+            protocol = "http://"
+
+        # control plane nodes
+        for index in range(len(ch)):
+            etcd_bootstrap_list.append( ch[index] + "=" + protocol + cpi[index] + ":" + str(peer_port) )
+
+        # worker nodes
+        for index in range(len(wh)):
+            etcd_bootstrap_list.append( wh[index] + "=" + protocol + whi[index] + ":" + str(peer_port) )
+
+        return ','.join(etcd_bootstrap_list)
+
+    def etcd_k3s_endpoint_builder(self, blankignore, cp_fqdn, worker_fqdn, client_port, ssl_enabled):
+        
+        ch = cp_fqdn.split(',')
+        wh = worker_fqdn.split(',')
+        etcd_k3s_endpoints = [ ]
+
+        if ssl_enabled:
+            protocol = "https://"
+        else:
+            protocol = "http://"
+
+        # control plane nodes
+        for index in range(len(ch)):
+            etcd_k3s_endpoints.append( protocol + ch[index] + ":" + str(client_port) )
+
+        # worker nodes
+        for index in range(len(wh)):
+            etcd_k3s_endpoints.append( protocol + wh[index] + ":" + str(client_port) )
+
+        return ','.join(etcd_k3s_endpoints)
